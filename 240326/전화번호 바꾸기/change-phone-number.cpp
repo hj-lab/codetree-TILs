8466ba7591dd8c52de00dd6ea8f5b57422433fb0
gamja <44892620+hj-lab@users.noreply.github.com>
2024-03-26 18:09:49 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int first, second, third;
+    char a = '-';
+    cin>>first;
+    cin.get();
+    cin>>second;
+    cin.get();
+    cin>>third;
+
+    cout<<0<<first<<a<<third<<a<<second<<endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
