a4d40f44105f369cd8b54b7314a198b843e02c9d
gamja <44892620+hj-lab@users.noreply.github.com>
2024-03-26 17:59:56 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int month, day, year;
+    char a;
+    cin>>month>>a>>day>>a>>year;
+
+    a = '.';
+    cout<<year<<a<<month<<a<<day;
+    return 0;
+}
