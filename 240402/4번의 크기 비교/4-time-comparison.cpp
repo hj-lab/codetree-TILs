ca2c14298b24d08de92d50dcf7ce212894cdb769
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-02 00:52:02 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+
+    (a>b) ? cout<<1<<endl : cout<<0<<endl;
+    (a>c) ? cout<<1<<endl : cout<<0<<endl;
+    (a>d) ? cout<<1<<endl : cout<<0<<endl;
+    (a>e) ? cout<<1<<endl : cout<<0<<endl;
+    return 0;
+}
