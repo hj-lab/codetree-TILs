c01b765e09a962b828a50fbde8deefbae2285ba9
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-03 01:34:26 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int max = a;
+
+    if(a<b){
+        max = b;
+        if(max<c){
+            max = c;
+        }
+    }
+
+    cout<<max;
+    return 0;
+}
