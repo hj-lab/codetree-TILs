179e589f403e24b32d52d2c8db3d9055c02aa6bb
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-05 19:02:15 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240405/369 게임/369-game.cpp
+++ 240405/369 게임/369-game.cpp
@@ -7,6 +7,8 @@ int main() {
     int one = n%10; // 일의자리
     int ten = (n%100 - one)/10; // 십의자리
 
+    cout<<one<<" "<<ten<<" "<<endl;
+
     for(int i=1; i<n+1; i++){
         if((i%3==0)|| (i%10 == one||ten) || ((i%100-i%10)/10==one||ten) ){
             cout<<"0"<<" ";
