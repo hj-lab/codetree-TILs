2ba000216ae363b7b0f1eb2805938e814dba0dee
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-07 01:58:22 +0900

    Title: 청소당번 정하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240407/청소당번 정하기/cleaning-numbering.cpp
+++ 240407/청소당번 정하기/cleaning-numbering.cpp
@@ -8,37 +8,6 @@ int main() {
     int corridorCnt = 0;
     int toiletCnt = 0;
 
-    // for(int i=1; i<day+1; i++){
-    //     if(i % 2 == 0){ // 2일마다 교실청소
-    //         if(i % 2 == 3){ //인데 2,3의 배수 = 복도 청소
-    //             if(i % 12 == 0){ // 인데 2,3,12의 배수 = 화장실 청소
-    //                 toiletCnt++;
-    //             }
-    //             else{
-    //                 corridorCnt++;
-    //             }
-    //         }
-    //         else{
-    //             classCnt++;
-    //         }
-    //     }
-    //     else if(i % 3 == 0){
-    //         if(i % 12 == 0){
-    //             toiletCnt++;
-    //         }
-    //         else{
-    //             corridorCnt++;
-    //         }
-    //     }
-    //     else if(i % 12 == 0){
-    //         toiletCnt++;
-    //     }
-        
-
-    // }
-    // i가 2의 배수 = 교실, 3의 배수 = 복도, 12의 배수=화장실
-    // 6의 배수 = 복도
-
     for(int i=1; i<=day; i++){
         if(i%12 == 0){
             toiletCnt++;
