91cfafd4a25f2d30594564ed28d1a311bfaca5bc
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-07 02:12:37 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240407/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    int sum_val = 0;
+
+    for(int i=a; i<b+1; i++){
+        sum_val += i;
+    }
+
+    cout<<sum_val;
+    return 0;
+}
