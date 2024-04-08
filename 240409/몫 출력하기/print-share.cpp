8d577f93c221f6c2892df1a098bc7a17872dee91
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-09 01:02:10 +0900

    Title: 몫 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240409/몫 출력하기/print-share.cpp
+++ 240409/몫 출력하기/print-share.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 int main() {
     int cnt = 0;
-    while(cnt<=3){
+    while(cnt<3){
         int number;
         cin>>number;
 
         if(number%2 == 0){
             cout<<number/2<<endl;
         }
-
 
         cnt++;
     }
