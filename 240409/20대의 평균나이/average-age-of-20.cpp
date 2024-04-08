995f2703f8a17378803ee169e3c067111ded9e75
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-09 01:09:19 +0900

    Title: 20대의 평균나이, Time: 1008ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240409/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<fixed;
+    int sum = 0;
+    int cnt = 0;
+    while(true){
+        int age;
+        cin>>age;
+
+        if(age>=30){
+            cout.precision(2);
+            cout<<(double)sum/cnt;
+            break;
+        }
+
+        sum += age;
+        cnt++;
+
+    }
+    return 0;
+}
