769bfb69f2d2eddbd3bdeddf234569e8964bc397
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-08 23:41:42 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/1부터의 합/sum-from-the-one.cpp
+++ 240408/1부터의 합/sum-from-the-one.cpp
@@ -6,11 +6,13 @@ int main() {
     int sum = 0;
 
     for(int i = 1 ; i < 101 ; i++){
-        if(sum+i >= n){
+        sum += i;
+
+        if(sum >= n){
+            cout<<i;
             break;
         }
-        sum += i;
+
     }
-    cout<<sum;
     return 0;
 }
