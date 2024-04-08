c0a51c5214deb8054bdbc131eee2898dcea12233
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-09 00:55:47 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240409/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    while(true){
+        int number;
+        cin>>number;
+
+        if(number == 0){
+            break;
+        }
+
+        cout<<number<<endl;
+    }
+    return 0;
+}
