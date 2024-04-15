6fa47c89b7dbbf78260b7a58986496030f1e6857
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-16 00:34:01 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/상하로 반복하며 출력/print-in-up-down-reverse.cpp
+++ 240416/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -7,7 +7,7 @@ int main() {
     //홀수열은 1,2,3,4 / 짝수열은 4,3,2,1
 
     for(int i=0; i<n; i++){
-        for(int j=n; j>0; j--){
+        for(int j=0; j<n; j++){
             if(j%2 == 0){
                 cout<<i+1;
             }
