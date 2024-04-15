a8613f8529bcad5609bc789fe875cb2d0975ac9e
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-15 13:36:48 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240415/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    int cnt = 1;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(cnt > 9){
+                cnt = 1;
+            }
+            cout<<cnt;
+            cnt++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
