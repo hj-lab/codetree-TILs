faabf9456e1539880b0bc1312fc8798deda537b8
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-17 19:17:43 +0900

    Title: 모양대로 숫자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240417/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=n; i>0; i--){
+        for(int j=n; j>=1; j--){
+            if(j>i){
+                cout<<"  ";
+            }
+            else{
+                cout<<j<<" ";
+            }
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
