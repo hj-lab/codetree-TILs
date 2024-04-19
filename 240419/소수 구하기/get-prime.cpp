75eb68d7b9cfe9271d75944ef3055cf371767dc5
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-19 20:40:36 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240419/소수 구하기/get-prime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=2; i<=n; i++){
+        bool isPrime = true;
+        for(int j=2; j<i; j++){
+            if(i % j == 0){ // 약수가 없으면
+                isPrime = false;
+            }
+        }
+            if(isPrime == true){
+                cout<<i<<" ";
+            }
+
+    }
+    return 0;
+}
