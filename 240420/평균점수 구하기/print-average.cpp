1c16cc124fa32fe4dd3e886cfa74e5cbd2f4c6e8
gamja <44892620+hj-lab@users.noreply.github.com>
2024-04-20 19:32:48 +0900

    Title: 평균점수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240420/평균점수 구하기/print-average.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double score[8] = {};
+    double sum = 0;
+    double average = 0;
+
+    for(int i=0; i<8; i++){
+        cin>>score[i];
+
+        sum += score[i];
+    }
+
+    average = sum / 8;
+
+    cout<<average;
+    return 0;
+}
