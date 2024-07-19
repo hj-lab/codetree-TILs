04b3928c5467e659570fa3775cf800b03a21f900
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-19 18:17:15 +0900

    Title: 주사위 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/주사위 놀이/play-with-dice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int count_arr[7] = {0}; // 첫번째는 비워두고
+
+    for(int i=0; i<10; i++){
+        cin>>arr[i];
+        count_arr[arr[i]]++;
+    }
+
+    for(int i=1; i<7; i++){
+        cout<<i<<" - "<<count_arr[i]<<endl;
+    }
+    return 0;
+}
