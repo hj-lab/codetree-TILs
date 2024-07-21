d263727fc3c21caf37ca7925b9feac007d782875
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-21 18:38:04 +0900

    Title: 연속부분수열일까, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240721/연속부분수열일까/contiguous-array-or-not.cpp
+++ 240721/연속부분수열일까/contiguous-array-or-not.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    return 0;
-} vice versa
-raphalelohsiyeol02@naver.com
-#include std
+int main() { 
+    int a,b;
+    cin>>a>>b;
+    int arrA[a];
+    int arrB[b];
+
+    for(int i=0; i<a; i++){
+        cin>>arrA[i];
+    }
+    for(int i=0; i<b; i++){
+        cin>>arrB[i];
+    }
+
+    // b의 첫번째 원소가 나오는 index를 저장해놓고 거기부터 따져보기
+    bool isRight = false;
+    // a의 N번째에 b의 첫번째 원소가 있음을 담고 있다
+    int isSequence[b] = {0,};
+
+    for(int i=0; i<a; i++){
+        if(arrA[i] == arrB[0]){
+            isSequence[i] = i;
+        }
+    }
+
+    for(int i=0; i<b; i++){
+        if(arrA[isSequence[i]] != arrB[i]){
+            break;
+        }
+        else{
+            isRight = true;
+        }
+    }
+
+    if(isRight == true){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+
+
+
+ return 0;
+}
