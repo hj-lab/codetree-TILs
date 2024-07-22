825566ea9d1d99681b72557eb683ce94b4736677
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-22 23:23:10 +0900

    Title: 999 또는 -999, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/999 또는 -999/999-or-999.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int max = -1000;
+    int min = 1000;
+
+    for(int i=0; i<100; i++){
+        int number;
+        cin>>number;
+        if(number == 999 || number == -999){
+            break;
+        }
+
+        if(max < number){
+            max = number;
+        }
+        else if(min > number){
+            min = number;
+        }
+    }
+    cout<<max<<" "<<min;
+    return 0;
+}
