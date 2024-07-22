06470f6647c665d8019738b07c1ee26c01d1cff6
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-22 23:32:48 +0900

    Title: n개의 숫자 중 최대 2개, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    int arr[N];
+    int max_first = INT_MIN;
+    int max_second = INT_MIN;
+
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+
+        if(arr[i] > max_first){
+            max_first = arr[i];
+        }
+        if(arr[i] > max_second && arr[i] < max_first){
+            max_second = arr[i];
+        }
+    }
+
+    cout<<max_first<<" "<<max_second;
+
+    return 0;
+}
