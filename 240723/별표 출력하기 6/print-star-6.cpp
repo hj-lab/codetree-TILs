36facf8171fe875f87d4470eb24773432fda821c
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-23 00:39:22 +0900

    Title: 별표 출력하기 6, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/별표 출력하기 6/print-star-6.cpp
+++ 240723/별표 출력하기 6/print-star-6.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void print_stars(int n) {
-    // 위쪽 부분
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            cout << " ";
-        }
-        for (int j = 0; j < 2 * (n - i) - 1; ++j) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-    
-    // 아래쪽 부분
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = 0; j < i; ++j) {
-            cout << " ";
-        }
-        for (int j = 0; j < 2 * (n - i) - 1; ++j) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-}
-
 int main() {
     int n;
     cin >> n;
-    print_stars(n);
+
+    // 위쪽 부분 출력
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            cout << "  "; // 앞부분 공백 출력
+        }
+        for (int j = 0; j < 2 * (n - i) - 1; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // 아래쪽 부분 출력
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = 0; j < i; ++j) {
+            cout << "  "; // 앞부분 공백 출력
+        }
+        for (int j = 0; j < 2 * (n - i) - 1; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
