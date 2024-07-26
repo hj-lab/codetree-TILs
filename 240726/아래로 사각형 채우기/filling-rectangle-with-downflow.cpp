954a1b7c2206f1e0d6d826864f34707eaab6c9eb
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-26 23:43:18 +0900

    Title: 아래로 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n][n];
+
+    for(int i=0; i<n; i++){
+        int row = i+1;
+        for(int j=0; j<n; j++){
+            arr[i][j] = row;
+            row += n;
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
