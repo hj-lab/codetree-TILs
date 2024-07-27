ecaeb727a908dc8e766551d5d86f8ac09e28bcb4
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-27 22:13:03 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240727/격자 반대로 채우기/grid-anti-fill.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 마지막 열부터 채우는데 홀수열일 경우 - 위에서 아래 / 짝수열일 경우 - 아래에서 위
+    int n;
+    cin>>n;
+    int arr[n][n];
+    int number = 1;
+
+    for(int i=n-1; i>=0; i--){
+        if(i%2 == 0){ // 홀수열일 경우
+            for(int j=0; j<n; j++){
+                arr[j][i] = number;
+                number++;
+            }
+        }
+        else{
+            for(int j=n-1; j>=0; j--){
+                arr[j][i] = number;
+                number++;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
