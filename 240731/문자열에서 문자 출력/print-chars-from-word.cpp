36d4a46def4b0116537df0e88e505c40f0a65872
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-31 20:33:24 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string me;
+    cin>>me;
+
+    for(int i=0; i<me.length(); i++){
+        cout<<me[i]<<endl;
+    }
+    return 0;
+}
