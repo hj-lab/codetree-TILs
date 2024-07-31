121f554ee76c75845781ae2265f580dfae5971ef
gamja <44892620+hj-lab@users.noreply.github.com>
2024-07-31 20:43:29 +0900

    Title: 문자열 범위 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240731/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -6,16 +6,11 @@ int main() {
     int len;
     cin>>voc>>len;
 
-    if(voc.length() < len){
-        for(int i=voc.length()-1; i>=0; i--){
-            cout<<voc[i];
-        }
-    }
-    else{
-        for(int i=voc.length()-1; i>=voc.length()-len; i--){
-            cout<<voc[i];
-        } 
-    }
+    int reverse = voc.length() < len ? 0 : voc.length()-len;
+
+    for(int i=voc.length()-1; i>=reverse; i--){
+        cout<<voc[i];
+    } 
 
     return 0;
 }
